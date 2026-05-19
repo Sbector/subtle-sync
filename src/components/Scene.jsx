08d24cbcc18ee7ca7af9a78103de17f2516e9627
No_otra_vez <saulbector@gmail.com>
2026-05-19 09:03:26 -0600

    Update buttons

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -483,8 +483,8 @@ export default function Scene() {
                         /* Speaker with sound waves */
                         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
-                            <path d="M15.54 3.54a8 8 0 0 1 0 11.32"></path>
-                            <path d="M19.07 4.93a16 16 0 0 1 0 22.63"></path>
+                            <path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path>
+                            <path d="M19.07 4.93a10 10 0 0 1 0 14.14"></path>
                         </svg>
                     ) : (
                         /* Speaker muted */
